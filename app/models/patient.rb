01e333c5b335e1a77e82adc1ddf74ef02772776d
Sergio García <sergio.garcia.sanjuan@gmail.com>
2024-08-09 12:50:33 +0200

    Adding Patient, fade data and annotate gem

---
+++ app/models/patient.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  birth_date :string
+#  phone      :string
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Patient < ApplicationRecord
+
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+1000.times do
+  patient = Patient.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birth_date: Faker::Date.birthday(min_age: 0, max_age: 65),
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address
+  )
+end
