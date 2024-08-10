b82999e87190dfda1a88e4c8f7715c3abbecbe12
Sergio García <sergio.garcia.sanjuan@gmail.com>
2024-08-10 18:56:04 +0200

    feature/EA-0002-add-fontawesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+# pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.6.0
+pin '@fortawesome/fontawesome-free', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js'
