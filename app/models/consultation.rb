f96c30ab56f8c4593ac53b5fee8f1338fac6c10a
Sergio García <sergio.garcia.sanjuan@gmail.com>
2024-09-12 12:44:14 +0200

    rack mini profiler added

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: consultations
+#
+#  id            :bigint           not null, primary key
+#  date          :date
+#  notes_after   :text
+#  notes_before  :text
+#  notes_to_send :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  patient_id    :bigint           not null
+#
+# Indexes
+#
+#  index_consultations_on_patient_id  (patient_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (patient_id => patients.id)
+#
 class Consultation < ApplicationRecord
   belongs_to :patient
   has_rich_text :notes_before
